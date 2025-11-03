42fb598d2c3407d9ea915d1808f10ffd23b63339
Adam Cohill <adamcohill16@gmail.com>
2025-11-02 19:55:23 -0500

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = {styles.text}>
+        HI my name is Adam Cogill, and i am a mobile developer.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
